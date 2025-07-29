6d1f37c446e8a93ef0120dd8222d53683a154f70
jamieseah <jamseah@gmail.com>
2025-07-29 22:49:19 +0800

    Lesson 5
    Lesson 5 documents

---
+++ lesson4/exercise1.js
@@ -0,0 +1,23 @@
+const MSFTStockClosingPrices = [
+{
+	Monday: 254.12,
+	Tuesday: 253.12,
+	Wednesday: 256.97,
+	Thursday: 257.52,
+	Friday: 258.93
+},
+{
+	Monday: 292.20,
+	Tuesday: 289.73,
+	Wednesday: 291.04,
+	Thursday: 294.55,
+	Friday: 296.29
+}]
+
+// Write your code below
+
+let sumMSFTStockClosingPrices = 0;
+for (let key in MSFTStockClosingPrices) {
+    
+    }
+}
